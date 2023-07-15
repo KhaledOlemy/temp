4ff618be65ed2c4ac2f66466d4cb481bdd964bf4
khaledolemy <khaledolemy@gmail.com>
2023-07-15 13:02:52 +0300

    remove all

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-import re
-import math
-# preparatory tip for extra guidance
-
-
-number = 10
-
-def adder(number, value=4):
-    out = number + value
-    return out
-
-a = b + c
-e = c * f + 405
-adder(26)
-print('git is awesome')
+# open world
